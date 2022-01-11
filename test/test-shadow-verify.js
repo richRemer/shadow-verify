422e9e199ea626452e7ba2e219742c68c2e47965
Richard Remer <rich.remer@gmail.com>
2022-01-11 09:21:44 -0800

    better naming in test

--- test/test-shadow-verify.js
+++ test/test-shadow-verify.js
@@ -3,11 +3,11 @@ import expect from "expect.js";
 
 describe("shadow-verify(string, string)", () => {
   const realValue = "foo";
-  const badValue = "bar";
+  const fakeValue = "bar";
   const hash = "$6$V9txQQkn$widChfOgAdGU1dUPVctmdRlM1aAZolyoxS5JhoqMw0c4FjgXVyHmbSoNj4ku/uGxc/rnsyC55.nEBhzfT/HEe1";
 
   it("should test if second value is hash of first", () => {
     expect(shadowVerify(realValue, hash)).to.be(true);
-    expect(shadowVerify(badValue, hash)).to.be(false);
+    expect(shadowVerify(fakeValue, hash)).to.be(false);
   });
 });
