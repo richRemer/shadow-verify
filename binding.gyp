b83a4bfef5e8fb853d66d33d325401037bff1210
Richard Remer <rich.remer@gmail.com>
2022-01-11 08:46:09 -0800

    use NAN to maintain Node.js version compatibility

--- binding.gyp
+++ binding.gyp
@@ -3,6 +3,7 @@
         {
             "target_name": "shadow",
             "sources": ["shadow.cc"],
+            "include_dirs": ["node_modules/nan"],
             "link_settings": {
                 "libraries": [
                     "-lcrypt"
--- shadow.cc
+++ shadow.cc
@@ -1,30 +1,30 @@
-#include <node.h>
+#include <nan.h>
 #include <unistd.h>
 
-namespace shadowVerify {
-  using v8::FunctionCallbackInfo;
-  using v8::Isolate;
-  using v8::Local;
-  using v8::Object;
-  using v8::String;
-  using v8::Value;
+using v8::FunctionTemplate;
+using v8::Isolate;
+using v8::String;
+using Nan::GetFunction;
+using Nan::New;
+using Nan::Set;
 
-  void Method(const FunctionCallbackInfo<Value>& args) {
-    Isolate* isolate = args.GetIsolate();
+NAN_METHOD(Verify) {
+  Isolate* isolate = info.GetIsolate();
 
-    args
-      .GetReturnValue()
-      .Set(
-        String::NewFromUtf8(
-          isolate,
-          crypt("bar", "$6$V9txQQkn$widChfOgAdGU1dUPVctmdRlM1aAZolyoxS5JhoqMw0c4FjgXVyHmbSoNj4ku/uGxc/rnsyC55.nEBhzfT/HEe1")
-        ).ToLocalChecked()
-      );
-  }
-
-  void Initialize(Local<Object> exports) {
-    NODE_SET_METHOD(exports, "verify", Method);
-  }
-
-  NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+  info
+    .GetReturnValue()
+    .Set(String::NewFromUtf8(
+      isolate,
+      crypt("foo", "$6$V9txQQkn$widChfOgAdGU1dUPVctmdRlM1aAZolyoxS5JhoqMw0c4FjgXVyHmbSoNj4ku/uGxc/rnsyC55.nEBhzfT/HEe1")
+    ).ToLocalChecked()
+  );
 }
+
+NAN_MODULE_INIT(InitAll) {
+  auto name = New<String>("verify").ToLocalChecked();
+  auto function = GetFunction(New<FunctionTemplate>(Verify)).ToLocalChecked();
+
+  Set(target, name, function);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, InitAll)
