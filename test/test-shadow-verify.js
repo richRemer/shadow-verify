25fb4bb3659d24d030290f292cd99a7bfeb70e31
Richard Remer <rich.remer@gmail.com>
2022-01-11 09:30:18 -0800

    add tests for bad usage

--- test/test-shadow-verify.js
+++ test/test-shadow-verify.js
@@ -10,4 +10,15 @@ describe("shadow-verify(string, string)", () => {
     expect(shadowVerify(realValue, hash)).to.be(true);
     expect(shadowVerify(fakeValue, hash)).to.be(false);
   });
+
+  it("should throw if secret is missing or not a string", () => {
+    expect(() => shadowVerify()).to.throwError();
+    expect(() => shadowVerify(42, hash)).to.throwError();
+  });
+
+  it("should throw if hash is missing, is not a string, or is empty", () => {
+    expect(() => shadowVerify(realValue)).to.throwError();
+    expect(() => shadowVerify(realValue, 42)).to.throwError();
+    expect(() => shadowVerify(realValue, "")).to.throwError();
+  });
 });
